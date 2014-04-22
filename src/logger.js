fd2b8b0a72e57cc3bea0b8de45b7d879345d0ee3
JoveX <xuyq1112@gmail.com>
2014-04-23 01:51:34 +0800

    add setTrackerData function

--- src/logger.js
+++ src/logger.js
@@ -211,6 +211,214 @@ if (typeof AMapLog !== 'object') {
             return urldecode(encodeWrapper(argString));
         }
 
+
+
+        /************************************************************
+         * sha1
+         * - based on sha1 from http://phpjs.org/functions/sha1:512 (MIT / GPL v2)
+         ************************************************************/
+
+        function sha1(str) {
+            // +   original by: Webtoolkit.info (http://www.webtoolkit.info/)
+            // + namespaced by: Michael White (http://getsprink.com)
+            // +      input by: Brett Zamir (http://brett-zamir.me)
+            // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+            // +   jslinted by: Anthon Pang (http://piwik.org)
+
+            var
+                rotate_left = function (n, s) {
+                    return (n << s) | (n >>> (32 - s));
+                },
+
+                cvt_hex = function (val) {
+                    var strout = '',
+                        i,
+                        v;
+
+                    for (i = 7; i >= 0; i--) {
+                        v = (val >>> (i * 4)) & 0x0f;
+                        strout += v.toString(16);
+                    }
+
+                    return strout;
+                },
+
+                blockstart,
+                i,
+                j,
+                W = [],
+                H0 = 0x67452301,
+                H1 = 0xEFCDAB89,
+                H2 = 0x98BADCFE,
+                H3 = 0x10325476,
+                H4 = 0xC3D2E1F0,
+                A,
+                B,
+                C,
+                D,
+                E,
+                temp,
+                str_len,
+                word_array = [];
+
+            str = utf8_encode(str);
+            str_len = str.length;
+
+            for (i = 0; i < str_len - 3; i += 4) {
+                j = str.charCodeAt(i) << 24 | str.charCodeAt(i + 1) << 16 |
+                    str.charCodeAt(i + 2) << 8 | str.charCodeAt(i + 3);
+                word_array.push(j);
+            }
+
+            switch (str_len & 3) {
+            case 0:
+                i = 0x080000000;
+                break;
+            case 1:
+                i = str.charCodeAt(str_len - 1) << 24 | 0x0800000;
+                break;
+            case 2:
+                i = str.charCodeAt(str_len - 2) << 24 | str.charCodeAt(str_len - 1) << 16 | 0x08000;
+                break;
+            case 3:
+                i = str.charCodeAt(str_len - 3) << 24 | str.charCodeAt(str_len - 2) << 16 | str.charCodeAt(str_len - 1) << 8 | 0x80;
+                break;
+            }
+
+            word_array.push(i);
+
+            while ((word_array.length & 15) !== 14) {
+                word_array.push(0);
+            }
+
+            word_array.push(str_len >>> 29);
+            word_array.push((str_len << 3) & 0x0ffffffff);
+
+            for (blockstart = 0; blockstart < word_array.length; blockstart += 16) {
+                for (i = 0; i < 16; i++) {
+                    W[i] = word_array[blockstart + i];
+                }
+
+                for (i = 16; i <= 79; i++) {
+                    W[i] = rotate_left(W[i - 3] ^ W[i - 8] ^ W[i - 14] ^ W[i - 16], 1);
+                }
+
+                A = H0;
+                B = H1;
+                C = H2;
+                D = H3;
+                E = H4;
+
+                for (i = 0; i <= 19; i++) {
+                    temp = (rotate_left(A, 5) + ((B & C) | (~B & D)) + E + W[i] + 0x5A827999) & 0x0ffffffff;
+                    E = D;
+                    D = C;
+                    C = rotate_left(B, 30);
+                    B = A;
+                    A = temp;
+                }
+
+                for (i = 20; i <= 39; i++) {
+                    temp = (rotate_left(A, 5) + (B ^ C ^ D) + E + W[i] + 0x6ED9EBA1) & 0x0ffffffff;
+                    E = D;
+                    D = C;
+                    C = rotate_left(B, 30);
+                    B = A;
+                    A = temp;
+                }
+
+                for (i = 40; i <= 59; i++) {
+                    temp = (rotate_left(A, 5) + ((B & C) | (B & D) | (C & D)) + E + W[i] + 0x8F1BBCDC) & 0x0ffffffff;
+                    E = D;
+                    D = C;
+                    C = rotate_left(B, 30);
+                    B = A;
+                    A = temp;
+                }
+
+                for (i = 60; i <= 79; i++) {
+                    temp = (rotate_left(A, 5) + (B ^ C ^ D) + E + W[i] + 0xCA62C1D6) & 0x0ffffffff;
+                    E = D;
+                    D = C;
+                    C = rotate_left(B, 30);
+                    B = A;
+                    A = temp;
+                }
+
+                H0 = (H0 + A) & 0x0ffffffff;
+                H1 = (H1 + B) & 0x0ffffffff;
+                H2 = (H2 + C) & 0x0ffffffff;
+                H3 = (H3 + D) & 0x0ffffffff;
+                H4 = (H4 + E) & 0x0ffffffff;
+            }
+
+            temp = cvt_hex(H0) + cvt_hex(H1) + cvt_hex(H2) + cvt_hex(H3) + cvt_hex(H4);
+
+            return temp.toLowerCase();
+        }
+
+        /************************************************************
+         * end sha1
+         ************************************************************/
+
+
+        /*
+         * Fix-up URL when page rendered from search engine cache or translated page
+         */
+        function urlFixup(hostName, href, referrer) {
+            if (hostName === 'translate.googleusercontent.com') {       // Google
+                if (referrer === '') {
+                    referrer = href;
+                }
+
+                href = getParameter(href, 'u');
+                hostName = getHostName(href);
+            } else if (hostName === 'cc.bingj.com' ||                   // Bing
+                    hostName === 'webcache.googleusercontent.com' ||    // Google
+                    hostName.slice(0, 5) === '74.6.') {                 // Yahoo (via Inktomi 74.6.0.0/16)
+                href = documentAlias.links[0].href;
+                hostName = getHostName(href);
+            }
+
+            return [hostName, href, referrer];
+        }
+
+        /*
+         * Fix-up domain
+         */
+        function domainFixup(domain) {
+            var dl = domain.length;
+
+            // remove trailing '.'
+            if (domain.charAt(--dl) === '.') {
+                domain = domain.slice(0, dl);
+            }
+
+            // remove leading '*'
+            if (domain.slice(0, 2) === '*.') {
+                domain = domain.slice(1);
+            }
+
+            return domain;
+        }
+
+        /*
+         * Title fixup
+         */
+        function titleFixup(title) {
+            title = title && title.text ? title.text : title;
+
+            if (!isString(title)) {
+                var tmp = documentAlias.getElementsByTagName('title');
+
+                if (tmp && isDefined(tmp[0])) {
+                    title = tmp[0].text;
+                }
+            }
+
+            return title;
+        }
+
         /************************************************************
          * Proxy object
          * - this allows the caller to continue push()'ing to _amapaq
@@ -227,8 +435,20 @@ if (typeof AMapLog !== 'object') {
 
             var
                 // 发送日志的路径
-                configTrackerUrl,
-                configProduct
+                configTrackerUrl
+                , configProduct
+                , trackerData = []
+                // First-party cookie name prefix
+                , configCookieNamePrefix = '_amap_'
+                // Current URL and Referrer URL
+                , locationArray = urlFixup(documentAlias.domain, windowAlias.location.href, getReferrer())
+                , domainAlias = domainFixup(locationArray[0])
+                , locationHrefAlias = locationArray[1]
+                , configReferrerUrl = locationArray[2]
+                , domainHash
+                , hash = sha1
+                , configCookieDomain
+                , configCookiePath
                 ;
 
 
@@ -263,6 +483,20 @@ if (typeof AMapLog !== 'object') {
             }
 
             /*
+             * Get cookie name with prefix and domain hash
+             */
+            function getCookieName(baseName) {
+                return configCookieNamePrefix + baseName + '.' + configProduct + '.' + domainHash;
+            }
+
+            /*
+             * Update domain hash
+             */
+            function updateDomainHash() {
+                domainHash = hash((configCookieDomain || domainAlias) + (configCookiePath || '/')).slice(0, 4); // 4 hexits = 16 bits
+            }
+
+            /*
              * 创建一个1x1的图片，向这个图片发送get请求
              */
             function getImage(request) {
@@ -274,6 +508,38 @@ if (typeof AMapLog !== 'object') {
                 image.src = configTrackerUrl + (configTrackerUrl.indexOf('?') < 0 ? '?' : '&') + request;
             }
 
+            /**
+             * 获取整个日志的search字符串
+             * @return {String} "a=1&b=2"
+             */
+            function getRequest () {
+            }
+
+            /**
+             * 拼装日志，并发送
+             */
+            function logEcommerce () {
+                var client_id = getClientId();
+                for (var i = trackerData.length - 1; i >= 0; i--) {
+                    console.log(trackerData[i]);
+                }
+                // console.log(client_id);
+            }
+
+            /**
+             * 获取用户id，单个用户的唯一标识
+             * @return {String} uuid
+             */
+            function getClientId () {
+                var client_id = getCookie(getCookieName('client_id'));
+                if (!client_id) {
+                    client_id = uuid();
+                    setCookie(getCookieName('client_id'), client_id);
+                }
+                return client_id;
+            }
+
+            updateDomainHash();
 
             return {
                 setTrackerUrl: function (trackerUrl) {
@@ -282,7 +548,13 @@ if (typeof AMapLog !== 'object') {
                 setProduct: function (product) {
                     configProduct = product;
                 },
+                setTrackerData: function (obj) {
+                    console.log(obj);
+                    trackerData.push(obj);
+                },
                 trackPageView: function () {
+                    // getRequest();
+                    logEcommerce();
                 }
             };
         }

911e112abf8230a3b97504a64a51c9e182069ab3
JoveX <xuyq1112@gmail.com>
2014-04-24 13:37:26 +0800

    add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: ['src/json2.js', 'src/uuid.js', 'src/logger.js'],
+                dest: 'tmp/<%= pkg.name %>.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: ['tmp/<%= pkg.name %>.js'],
+                dest: 'build/<%= pkg.name %>.min.js'
+            }
+        },
+        clean: {
+            tmp: ["tmp"]
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.registerTask('default', ['clean:tmp', 'concat', 'uglify', 'clean:tmp']);
+};
